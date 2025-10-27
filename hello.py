940201067dfb0f93a625a08545487330cb187753
pantale0n <pantale0n@yandex.ru>
2025-10-27 15:53:42 +0300

    Мое первое домашнее задание

---
+++ hello.py
@@ -0,0 +1,8 @@
+input_name_question = input("Привет, как тебя зовут? ") 
+name = str(input_name_question)
+
+input_age_question = input("Сколько тебе лет? ")
+age = str(input_age_question)
+
+print(f"Привет, {name}")
+print(f"Тебе {age} лет")
