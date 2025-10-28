966a87625d4fe849f1fbc3573163986b05f196b4
pantale0n <pantale0n@yandex.ru>
2025-10-28 07:27:14 +0300

    Начал выполнять перовое задание из второго дз

--- hello.py
+++
@@ -1,8 +0,0 @@
-input_name_question = input("Привет, как тебя зовут? ") 
-name = str(input_name_question)
-
-input_age_question = input("Сколько тебе лет? ")
-age = str(input_age_question)
-
-print(f"Привет, {name}")
-print(f"Тебе {age} лет")
