681cf50c3f19ded4dc7001179f53d397b653c833
pantale0n <pantale0n@yandex.ru>
2025-10-31 07:29:24 +0300

    Внес исправления в ДЗ Меню

--- menu.py
+++ menu.py
@@ -1,25 +1,25 @@
 category = str(input("Выберите категорию: напиток, суп, десерт: "))
 
 match category:
-    case "Напиток":
+    case "напиток": # type: ignore
         tea_cena = 50
         cofe_cena = 110
         sok_cena = 210
         napitok = str(input("Выберите напиток: чай, кофе, сок:"))
-        if napitok == "Чай":
+        if napitok == "чай":
             print(f"Вы выбрали напиток чай, стоимость заказа {tea_cena}")
-        elif napitok == "Кофе":
+        elif napitok == "кофе":
             print(f"Вы выбрали напиток кофе, стоимость заказа {cofe_cena}")
         else:
              print(f"Вы выбрали напиток кофе, стоимость заказа {sok_cena}")
-    case "Суп":
+    case "суп": # type: ignore
         borsh_cena = 150
         si_cena = 210
         pure_cena = 180
         sup = str(input("Выберите суп: борщ, щи, суп-пюре:"))
-        if sup == "Борщ":
+        if sup == "борщ":
             print(f"Вы выбрали борщ, стоимость заказа {borsh_cena}")
-        elif sup == "Щи":
+        elif sup == "щи":
             print(f"Вы выбрали щи, стоимость заказа {si_cena}")
         else:
             print(f"Вы выбрали суп-пюре, стоимость заказа {pure_cena}")
@@ -28,9 +28,9 @@ match category:
         icecream_cena = 310
         fruct_cena = 410
         desert = str(input("Выберите десерт: торт, мороженое, фрукты:"))
-        if desert == "Борщ":
-            print(f"Вы выбрали борщ, стоимость заказа {tort_cena}")
-        elif desert == "Щи":
-            print(f"Вы выбрали щи, стоимость заказа {icecream_cena}")
+        if desert == "торт":
+            print(f"Вы выбрали торт, стоимость заказа {tort_cena}")
+        elif desert == "мороженое":
+            print(f"Вы выбрали мороженое, стоимость заказа {icecream_cena}")
         else:
-            print(f"Вы выбрали суп-пюре, стоимость заказа {fruct_cena}")
+            print(f"Вы выбрали фрукты, стоимость заказа {fruct_cena}")
